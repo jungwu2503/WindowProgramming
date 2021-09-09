ca640f9e890f3ba693d67eabe3d02599a1beaaaf
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2021-09-09 15:31:50 +0900

    Add files via upload
    매개 변수o 람다식

---
+++ MyFunctionalInterfaceExample2/src/MyFunctionalInterface.java
@@ -0,0 +1,6 @@
+// 두개 이상의 추상메소드가 선언된 인터페이스는 람다식으로 구현객체 생성 x
+// 하나의 추상메서드가 선언된 인터페이스만이 람다식의 타켓 타입
+@FunctionalInterface // 두개 이상의 추상 메소드가 선언되지 않도록 체킹하는 어노테이션
+public interface MyFunctionalInterface {
+	public void method(int x);
+}
